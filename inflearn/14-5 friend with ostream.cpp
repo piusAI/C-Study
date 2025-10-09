d5214aa2f7de278c94ce818b906990f3e190adc5
PIUS <hwangpiusjoon@gmail.com>
2025-10-09 12:40:38 +0900

    Update 14-5 friend with ostream.cpp

--- inflearn/14-5 friend with ostream.cpp
+++ inflearn/14-5 friend with ostream.cpp
@@ -10,6 +10,17 @@ public:
         os<< "("<<v.x<<","<<v.y<<")"<<endl;
         return os;
     }
+
+    // ostream은 사용자 수정 불가하기때문에
+    // my vector안에서 friend ostream& operator<<를 선언해 os<<v 수정 지원
+
+    // friend ostream& operator<< : MyVector 출력, private x, y 접근
+    // 연쇄 출력( cout<< v<< v2)를 위해 stream& 반환
+
+    // ostream& os를 매개변수로 받는 이유?
+    // cout << v => operator<<(cout, v)로 해석 가능
+    // ostream& os : cout
+    // Myvector &v : v
 };
 
 
