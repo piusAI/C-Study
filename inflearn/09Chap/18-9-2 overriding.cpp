b8b3d778d40d1164a92406a6b00829d385e12b43
PIUS <hwangpiusjoon@gmail.com>
2025-10-10 12:08:36 +0900

    Create 18-9-2 overriding.cpp

---
+++ inflearn/09Chap/18-9-2 overriding.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+    virtual void f() {cout << "Base ::f() called "<<endl;}
+};
+
+class Derived:public Base
+{
+public:
+    virtual void f() {cout<< "Derived ::f() called "<<endl;}
+};
+
+
+int main()
+{
+    Derived d, *pDer;
+
+    pDer=&d;
+    pDer->f();
+    
+    Base *pBase;
+    pBase = pDer;   // upcasting으로 Derive class를 가리킨다.
+                    // Base::f()가 virtual이므로, Derivde::f()가 overriding!
+    pBase->f();     // 런타임에 자식의 Derive ::f() 호출
+    
+}
