f113adc847501db7e1bbb1a07e0cd50eeecddcd2
PIUS <hwangpiusjoon@gmail.com>
2025-10-10 15:52:23 +0900

    Create 18-15 Rectangle virtualOverride.cpp

---
+++ inflearn/09Chap/18-15 Rectangle virtualOverride.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+    protected:
+    int x,y;
+public:
+    Shape(int x, int y):x(x),y(y){}
+    virtual void draw() //순수 가상 함수는 아니다잉!
+    {
+        cout<<"Shape Draw"<<endl;
+    }
+};
+
+class Rect:public Shape
+{
+private :
+    int width, height;
+public:
+    Rect(int x, int y, int width, int height):Shape(x,y), width(width), height(height){}
+    void draw()
+    {
+        cout<<"Rectangle Draw"<<endl;
+    }
+};
+
+int main()
+{
+    Shape *ps = new Rect(0,0, 100, 100); // Rect를 가리킨다
+    ps->draw(); //Rect인 자식 class의 draw가 시행되겠다!
+
+    delete ps;
+    return 0;
+}
